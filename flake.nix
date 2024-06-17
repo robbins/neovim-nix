4dfe5150d8d6b2ecbb12facf329825ab059b71f4
Nathanael Robbins <31457698+robbins@users.noreply.github.com>
2024-06-17 16:33:51 -0400

    flake: init nixvim template

---
+++ flake.nix
@@ -0,0 +1,37 @@
+{
+  description = "Nate's NixVim configuration";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
+    nixvim.url = "github:nix-community/nixvim";
+    flake-parts.url = "github:hercules-ci/flake-parts";
+  };
+
+  outputs =
+    { nixvim, flake-parts, ... }@inputs:
+    flake-parts.lib.mkFlake { inherit inputs; } {
+      systems = [
+        "x86_64-linux"
+        "aarch64-linux"
+        "aarch64-darwin"
+      ];
+
+      perSystem =
+        { pkgs, system, ... }:
+        let
+          cfg = nixvim.lib.evalNixvim {
+            inherit system;
+
+            modules = [ ./config ];
+
+            extraSpecialArgs = {
+            };
+          };
+        in
+        {
+          checks.default = cfg.config.build.test;
+          formatter.default = pkgs.nixfmt;
+          packages.default = cfg.config.build.package;
+        };
+    };
+}
