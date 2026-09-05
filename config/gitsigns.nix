185b2bad413f1ad3f67a14fcec09b2473ea5a4d1
Nathanael Robbins <31457698+robbins@users.noreply.github.com>
2026-09-05 14:44:06 -0400

    gitsigns: keymaps

--- config/gitsigns.nix
+++ config/gitsigns.nix
@@ -9,4 +9,47 @@
       };
     };
   };
+
+  keymaps = [
+    {
+      mode = "n";
+      key = "<leader>gb";
+      action = "<cmd>Gitsigns blame_line<CR>";
+    }
+    {
+      mode = "n";
+      key = "<leader>gB";
+      action = "<cmd>Gitsigns blame<CR>";
+    }
+    {
+      mode = "n";
+      key = "]h";
+      action = "<cmd>Gitsigns nav_hunk prev<CR>";
+    }
+    {
+      mode = "n";
+      key = "]h";
+      action = "<cmd>Gitsigns nav_hunk prev<CR>";
+    }
+    {
+      mode = "n";
+      key = "<leader>hs";
+      action = "<cmd>Gitsigns stage_hunk<CR>";
+    }
+    {
+      mode = "n";
+      key = "<leader>hr";
+      action = "<cmd>Gitsigns reset_hunk<CR>";
+    }
+    {
+      mode = "n";
+      key = "<leader>hR";
+      action = "<cmd>Gitsigns reset_buffer<CR>";
+    }
+    {
+      mode = "n";
+      key = "<leader>hp";
+      action = "<cmd>Gitsigns preview_hunk_inline<CR>";
+    }
+  ];
 }
