3e231c3e15e1cbbac29e4dfb9f4643dedee77594
Nathanael Robbins <31457698+robbins@users.noreply.github.com>
2026-09-19 23:51:15 -0400

    indent-blankline: configure

---
+++ config/indent-blankline.nix
@@ -0,0 +1,13 @@
+{
+  plugins.indent-blankline = {
+    enable = true;
+    settings = {
+      indent = {
+        highlight = [ "GruvboxBg1" ];
+      };
+      scope = {
+        show_end = false;
+      };
+    };
+  };
+}
