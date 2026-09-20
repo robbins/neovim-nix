611d8f6dfadf2f0703e1e91e1c843e0a2f807e46
Nathanael Robbins <31457698+robbins@users.noreply.github.com>
2026-09-20 00:30:31 -0400

    blink-pairs: configure

---
+++ config/blink-pairs.nix
@@ -0,0 +1,15 @@
+{
+  plugins.blink-pairs = {
+    enable = true;
+    settings = {
+      highlights = {
+        groups = [
+          "BlinkPairsPurple"
+          "BlinkPairsCyan"
+          "BlinkPairsYellow"
+          "BlinkPairsBlue"
+        ];
+      };
+    };
+  };
+}
